d33e525178db599ada66c7ae738b9b974b234b32
Srijan Kundu <srijan.kundu7@gmail.com>
2024-07-07 00:56:16 +0530

    July 07, 2024

--- Classification_v2/utils.py
+++ Classification_v2/utils.py
@@ -9,7 +9,6 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 CLASSES = 1
 
 def pauc_above_tpr(y_true, y_pred, min_tpr=0.80):
-    # Flip the labels and predictions as per the competition metric
     y_true = abs(np.array(y_true) - 1)
     y_pred = -1.0 * np.array(y_pred)
     
@@ -21,16 +20,21 @@ def pauc_above_tpr(y_true, y_pred, min_tpr=0.80):
     y_interp = [tpr[stop - 1], tpr[stop]]
     tpr = np.append(tpr[:stop], np.interp(max_fpr, x_interp, y_interp))
     fpr = np.append(fpr[:stop], max_fpr)
+    
+    if len(fpr) < 2:
+        print("Warning: Not enough points to compute pAUC. Returning 0.")
+        return 0
+    
     partial_auc = auc(fpr, tpr)
 
     return partial_auc
 
 def evaluate(loader, model):
     criterion = nn.BCEWithLogitsLoss()
-    metric = BinaryF1Score(threshold=0.5).cuda()
-    prec = BinaryPrecision(threshold=0.5).cuda()
-    recall = BinaryRecall(threshold=0.5).cuda()
-    acc = BinaryAccuracy(threshold=0.5).cuda()
+    metric = BinaryF1Score(threshold=0.5).to(DEVICE)
+    prec = BinaryPrecision(threshold=0.5).to(DEVICE)
+    recall = BinaryRecall(threshold=0.5).to(DEVICE)
+    acc = BinaryAccuracy(threshold=0.5).to(DEVICE)
     loss = 0.0
     num_corr = 0
     num_samp = 0
@@ -61,11 +65,9 @@ def evaluate(loader, model):
 
     return avg_loss, acc.compute().item(), prec.compute().item(), recall.compute().item(), metric.compute().item(), pauc
 
-
 def save_checkpoint(state, filename="my_checkpoint.pth"):
     print("=> Saving checkpoint")
     torch.save(state, filename)
-
 
 def load_checkpoint(checkpoint, model):
     print("=> Loading checkpoint")
