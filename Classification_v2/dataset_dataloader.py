d2137863ef1983f0f182810c4efa48014c1f1a8a
Srijan Kundu <srijan.kundu7@gmail.com>
2024-07-06 21:33:21 +0530

    July 06, 2024

--- Classification_v2/dataset_dataloader.py
+++ Classification_v2/dataset_dataloader.py
@@ -1,6 +1,7 @@
 import h5py
 import io
 import numpy as np
+import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader, Subset
 from torchvision.transforms import v2
@@ -10,7 +11,7 @@ from PIL import Image
 TRAIN_DATA_PATH = 'D:/ISIC 2024 - Skin Cancer Detection with 3D-TBP/Data/train-image.hdf5'
 TEST_DATA_PATH = 'D:/ISIC 2024 - Skin Cancer Detection with 3D-TBP/Data/test-image.hdf5'
 # METADATA_CSV_PATH_50kSAMPLE = 'D:/ISIC 2024 - Skin Cancer Detection with 3D-TBP/EDA/isic2024_50ksample.csv'
-# METADATA_CSV_PATH = "D:/ISIC 2024 - Skin Cancer Detection with 3D-TBP/Data/train-metadata.csv"
+METADATA_CSV_PATH = "D:/ISIC 2024 - Skin Cancer Detection with 3D-TBP/Data/train-metadata.csv"
 
 TRANS_TRAIN = v2.Compose([
     v2.Resize((224, 224)),
@@ -27,25 +28,82 @@ TRANS_TEST = v2.Compose([
 ])
 
 
+# class ISIC_2024_HDF5Dataset(Dataset):
+#     def __init__(self, file_path, label_df=None, transform=None):
+#         self.file_path = file_path
+#         self.transform = transform
+        
+#         with h5py.File(self.file_path, 'r') as file:
+#             all_keys = list(file.keys())
+        
+#         if label_df is not None:
+#             # Ensure 'isic_id' doesn't have the .jpg extension
+#             label_df['isic_id'] = label_df['isic_id'].str.replace('.jpg', '')
+            
+#             # Filter keys to only those present in the label_df
+#             self.keys = [key for key in all_keys if key in label_df['isic_id'].values]
+            
+#             # Create a dictionary for quick label lookup
+#             self.labels_dict = dict(zip(label_df['isic_id'], label_df['target']))
+#         else:
+#             self.keys = all_keys
+#             self.labels_dict = None
+    
+#     def __getitem__(self, index):
+#         with h5py.File(self.file_path, 'r') as file:
+#             key = self.keys[index]
+#             jpeg_data = file[key][()]
+#             image = Image.open(io.BytesIO(jpeg_data))
+#             image = np.array(image)
+        
+#         if self.transform:
+#             image = self.transform(image)
+        
+#         if self.labels_dict:
+#             label = self.labels_dict[key]
+#             return image, label
+#         else:
+#             return image, 0  # For test set without labels
+    
+#     def __len__(self):
+#         return len(self.keys)
+
+
+# def get_loader(label_df, batch_size=16, seed=None):
+#     train_dataset = ISIC_2024_HDF5Dataset(TRAIN_DATA_PATH, label_df=label_df, transform=TRANS_TRAIN)
+#     test_dataset = ISIC_2024_HDF5Dataset(TEST_DATA_PATH, transform=TRANS_TEST)
+    
+#     # Split train dataset into train and validation
+#     train_idx, val_idx = train_test_split(
+#         range(len(train_dataset)),
+#         test_size=0.2,
+#         stratify=[train_dataset[i][1] for i in range(len(train_dataset))],
+#         random_state=seed
+#     )
+    
+#     train_subset = Subset(train_dataset, train_idx)
+#     val_subset = Subset(train_dataset, val_idx)
+    
+#     train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=True)
+#     val_loader = DataLoader(val_subset, batch_size=batch_size, shuffle=False)
+#     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+#     return train_loader, val_loader, test_loader
+
+
 class ISIC_2024_HDF5Dataset(Dataset):
-    def __init__(self, file_path, label_df=None, transform=None):
+    def __init__(self, file_path, label_csv=None, transform=None):
         self.file_path = file_path
         self.transform = transform
         
         with h5py.File(self.file_path, 'r') as file:
-            all_keys = list(file.keys())
+            self.keys = list(file.keys())
         
-        if label_df is not None:
-            # Ensure 'isic_id' doesn't have the .jpg extension
-            label_df['isic_id'] = label_df['isic_id'].str.replace('.jpg', '')
-            
-            # Filter keys to only those present in the label_df
-            self.keys = [key for key in all_keys if key in label_df['isic_id'].values]
-            
-            # Create a dictionary for quick label lookup
-            self.labels_dict = dict(zip(label_df['isic_id'], label_df['target']))
+        if label_csv:
+            self.labels_df = pd.read_csv(label_csv, low_memory=False)
+            self.labels_df['isic_id'] = self.labels_df['isic_id'].str.replace('.jpg', '')
+            self.labels_dict = dict(zip(self.labels_df['isic_id'], self.labels_df['target']))
         else:
-            self.keys = all_keys
             self.labels_dict = None
     
     def __getitem__(self, index):
@@ -59,7 +117,7 @@ class ISIC_2024_HDF5Dataset(Dataset):
             image = self.transform(image)
         
         if self.labels_dict:
-            label = self.labels_dict[key]
+            label = self.labels_dict.get(key, -1)  # Use -1 if key not found
             return image, label
         else:
             return image, 0  # For test set without labels
@@ -67,9 +125,8 @@ class ISIC_2024_HDF5Dataset(Dataset):
     def __len__(self):
         return len(self.keys)
 
-
-def get_loader(label_df, batch_size=16, seed=None):
-    train_dataset = ISIC_2024_HDF5Dataset(TRAIN_DATA_PATH, label_df=label_df, transform=TRANS_TRAIN)
+def get_loader(batch_size=16, seed=None):
+    train_dataset = ISIC_2024_HDF5Dataset(TRAIN_DATA_PATH, label_csv=METADATA_CSV_PATH, transform=TRANS_TRAIN)
     test_dataset = ISIC_2024_HDF5Dataset(TEST_DATA_PATH, transform=TRANS_TEST)
     
     # Split train dataset into train and validation
