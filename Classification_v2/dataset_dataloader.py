95498ca38b078bfe33d5478b7338c9db02807de2
Srijan Kundu <srijan.kundu7@gmail.com>
2024-07-10 11:50:11 +0530

    test_3.1

---
+++ Classification_v2/dataset_dataloader.py
@@ -0,0 +1,231 @@
+'''
+Imports
+'''
+import h5py
+from io import BytesIO
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader, Subset
+from torchvision.transforms import v2
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from PIL import Image
+# import cv2
+from sklearn.model_selection import StratifiedShuffleSplit
+
+
+'''
+Paths
+'''
+## Kaggle
+# TRAIN_HDF5_PATH = "/kaggle/input/isic-2024-challenge/train-image.hdf5"
+# TEST_HDF5_PATH = "/kaggle/input/isic-2024-challenge/test-image.hdf5"
+# Local_Srijan
+TRAIN_HDF5_PATH = "D:\\ISIC 2024 - Skin Cancer Detection with 3D-TBP\\Data\\train-image.hdf5"
+TEST_HDF5_PATH = "D:\\ISIC 2024 - Skin Cancer Detection with 3D-TBP\\Data\\test-image.hdf5"
+## Local_Sruba
+# TRAIN_HDF5_PATH = "E:\\isic-2024-challenge\\Dataset\\train-image.hdf5"
+# TEST_HDF5_PATH = "E:\\isic-2024-challenge\\Dataset\\test-image.hdf5"
+
+
+'''
+Transformations
+'''
+'''Transformations using torchvision.transforms.v2'''
+data_transforms_v2 = {
+    "train": v2.Compose([
+        v2.Resize((224, 224)),
+        v2.ToImage(),
+        v2.RandomRotation(degrees=(0, 360)),
+        v2.RandomHorizontalFlip(p=0.5),
+        v2.ToDtype(torch.float32, scale = True)
+    ]),
+    "test": v2.Compose([
+        v2.Resize((224, 224)),
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale = True)
+    ])
+}
+
+
+data_transforms_album = {
+    "train": A.Compose([
+        A.Resize(224, 224),
+        A.RandomRotate90(p=0.5),
+        A.Flip(p=0.5),
+        A.Downscale(p=0.25),
+        A.ShiftScaleRotate(shift_limit=0.1, 
+                           scale_limit=0.15, 
+                           rotate_limit=60, 
+                           p=0.5),
+        A.HueSaturationValue(
+                hue_shift_limit=0.2, 
+                sat_shift_limit=0.2, 
+                val_shift_limit=0.2, 
+                p=0.5
+            ),
+        A.RandomBrightnessContrast(
+                brightness_limit=(-0.1,0.1), 
+                contrast_limit=(-0.1, 0.1), 
+                p=0.5
+            ),
+        A.Normalize(
+                mean=[0.485, 0.456, 0.406], 
+                std=[0.229, 0.224, 0.225], 
+                max_pixel_value=255.0, 
+                p=1.0
+            ),
+        ToTensorV2()], p=1.),
+    
+    "test": A.Compose([
+        A.Resize(224, 224),
+        A.Normalize(
+                mean=[0.485, 0.456, 0.406], 
+                std=[0.229, 0.224, 0.225], 
+                max_pixel_value=255.0, 
+                p=1.0
+            ),
+        ToTensorV2()], p=1.)
+}
+
+
+'''
+DataClass
+'''
+class ISIC2024_HDF5(Dataset):
+    '''
+    with augmentations using torchvision.transforms.v2
+    '''
+    def __init__(self, hdf5_path, annotations_df=None, transform=None):
+        self.hdf5_path = hdf5_path
+        self.annotations_df = annotations_df
+        self.transform = transform
+        self.image_ids = []
+        
+        self.hdf5_file = h5py.File(self.hdf5_path, 'r')
+
+        if self.annotations_df is not None:
+            self.image_ids = annotations_df['isic_id']
+            self.labels = annotations_df.set_index('isic_id')['target'].to_dict()
+        else:
+            self.image_ids = list(self.hdf5_file.keys())
+            
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]      # PIL
+        image = Image.open(BytesIO(self.hdf5_file[image_id][()]))
+        # image = self.load_image(self.hdf5_file[image_id][()])     # cv2
+
+        if self.transform:
+            image = self.transform(image)
+
+        # Check for NaN in image
+        if torch.isnan(image).any():
+            print(f"NaN detected in image {image_id}")
+
+        if self.annotations_df is not None:
+            label = self.labels[image_id]
+            # Check for NaN in label
+            if np.isnan(label):
+                print(f"NaN detected in label for image {image_id}")
+            return image, label, image_id
+        else:
+            return image, image_id
+        
+    # def load_image(self, image_data):                             # cv2
+    #     # Decode the image data from HDF5 file using OpenCV
+    #     image = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
+    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+    #     # image = np.transpose(image, (1, 2, 0))  # Convert HxWxC to CxHxW
+    #     return image
+    
+    def close(self):
+        self.hdf5_file.close()
+
+
+class ISIC2024_HDF5_ALBUM(Dataset):
+    '''
+    With augmentations using albumentations 
+    '''
+    def __init__(self, hdf5_path, annotations_df=None, transform=None):
+        self.hdf5_path = hdf5_path
+        self.annotations_df = annotations_df
+        self.transform = transform
+        self.image_ids = []
+        
+        self.hdf5_file = h5py.File(self.hdf5_path, 'r')
+
+        if self.annotations_df is not None:
+            self.image_ids = annotations_df['isic_id']
+            self.labels = annotations_df.set_index('isic_id')['target'].to_dict()
+        else:
+            self.image_ids = list(self.hdf5_file.keys())
+            
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        image = np.array(Image.open(BytesIO(self.hdf5_file[image_id][()])))
+        
+        # image = self.load_image(self.hdf5_file[image_id][()])
+
+        if self.transform:
+            # image = self.transform(image)
+            image = self.transform(image=image)["image"]        # Albumentations returns a dictionary with keys like 'image', 'mask', etc., depending on the transformations applied.
+
+        # Check for NaN in image
+        if torch.isnan(image).any():
+            print(f"NaN detected in image {image_id}")
+
+        if self.annotations_df is not None:
+            label = self.labels[image_id]
+            # Check for NaN in label
+            if np.isnan(label):
+                print(f"NaN detected in label for image {image_id}")
+            return image, label, image_id
+        else:
+            return image, image_id
+        
+    # def load_image(self, image_data):
+    #     # Decode the image data from HDF5 file using OpenCV
+    #     image = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
+    #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+    #     # image = np.transpose(image, (1, 2, 0))  # Convert HxWxC to CxHxW
+    #     return image
+    
+    def close(self):
+        self.hdf5_file.close()
+
+
+'''
+DataLoader
+'''
+def get_loader(train_labels_df, 
+               dataset_cls=ISIC2024_HDF5_ALBUM,
+               train_hdf5_path=TRAIN_HDF5_PATH, 
+               test_hdf5_path=TEST_HDF5_PATH, 
+               train_img_trans=data_transforms_album["train"], 
+               test_img_trans=data_transforms_album["test"], 
+               batch=32, 
+               seed=None):
+    
+    train_dataset_all = dataset_cls(hdf5_path=train_hdf5_path, annotations_df=train_labels_df, transform=train_img_trans)
+    test_dataset = dataset_cls(hdf5_path=test_hdf5_path, transform=test_img_trans)
+
+    train_annotations_all = train_labels_df
+    labels = train_annotations_all['target']
+    splitter = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=seed)
+    train_idx, val_idx = next(splitter.split(train_annotations_all, labels))
+    train_subset = Subset(train_dataset_all, train_idx)
+    val_subset = Subset(train_dataset_all, val_idx)
+
+    train_loader = DataLoader(train_subset, batch_size=batch, shuffle=True)
+    val_loader = DataLoader(val_subset, batch_size=batch, shuffle=True)
+    test_loader = DataLoader(test_dataset, shuffle=False)
+    
+    return train_loader, val_loader, test_loader
---
+++ Classification_v2/utils.py
@@ -0,0 +1,246 @@
+'''
+Imports
+'''
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+import torch, torchvision
+import torch.nn as nn
+from torch.optim import lr_scheduler
+from sklearn.metrics import roc_curve, auc
+from torchmetrics.classification import BinaryAccuracy, BinaryPrecision, BinaryRecall, BinaryF1Score
+import matplotlib.pyplot as plt
+
+'''
+Constants
+'''
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+
+def train(epochs, model, learning_rate, train_dl, val_dl, min_epoch_train, patience, log_file, model_save_path, criterion = nn.BCEWithLogitsLoss()):
+    '''
+    Training function for ISIC-2024 competition data
+    Parameters:
+        epochs: Number of epoches
+        model: Model 
+        learning_rate: model learning rate
+        train_dl: Training data loader
+        val_dl: Validation data loader
+        min_epoch_train: Train for minimum epoches after which early-stopping kicks in
+        patience: Patience for early-stopping
+        log_file: log file location to save logs
+        model_save_path: location for saving trained model 
+        criterion: Loss function, defaults to BCE with logit loss function
+    '''
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs)  # Initialize CosineAnnealingLR scheduler
+    scaler = torch.cuda.amp.GradScaler()
+    
+    pass
+
+
+def pauc_above_tpr(y_true, y_pred, min_tpr=0.80):
+    '''
+    Custom metric according to competition
+    https://en.wikipedia.org/wiki/Partial_Area_Under_the_ROC_Curve
+    https://www.kaggle.com/code/metric/isic-pauc-abovetpr
+    '''
+    y_true = abs(np.array(y_true) - 1)
+    y_pred = -1.0 * np.array(y_pred)
+    
+    # Check for NaN values
+    if np.isnan(y_true).any() or np.isnan(y_pred).any():
+        print("NaN values detected in inputs to pauc_above_tpr")
+        return 0
+    
+    fpr, tpr, _ = roc_curve(y_true, y_pred)
+    max_fpr = 1 - min_tpr
+
+    stop = np.searchsorted(fpr, max_fpr, "right")
+    x_interp = [fpr[stop - 1], fpr[stop]]
+    y_interp = [tpr[stop - 1], tpr[stop]]
+    tpr = np.append(tpr[:stop], np.interp(max_fpr, x_interp, y_interp))
+    fpr = np.append(fpr[:stop], max_fpr)
+    
+    if len(fpr) < 2:
+        print("Warning: Not enough points to compute pAUC. Returning 0.")
+        return 0
+    
+    partial_auc = auc(fpr, tpr)
+
+    return partial_auc
+
+
+def evaluate(loader, model, criterion):
+    '''
+    Evaluate function for validation set
+    '''
+    metric = BinaryF1Score(threshold=0.5).to(DEVICE)
+    prec = BinaryPrecision(threshold=0.5).to(DEVICE)
+    recall = BinaryRecall(threshold=0.5).to(DEVICE)
+    acc = BinaryAccuracy(threshold=0.5).to(DEVICE)
+    loss = 0.0
+    num_corr = 0
+    num_samp = 0
+    all_preds = []
+    all_labels = []
+    
+    model.eval()
+    with torch.no_grad():
+        for inputs, labels, _ in tqdm(loader):
+            inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
+            outputs = model(inputs).squeeze(1)
+            
+            # Check for NaN in outputs
+            if torch.isnan(outputs).any():
+                print("NaN detected in model outputs")
+                continue
+            
+            loss += criterion(outputs, labels.float()).item()
+            preds = torch.sigmoid(outputs)
+            num_corr += ((preds > 0.5) == labels).sum()
+            num_samp += preds.size(0)
+            metric.update(preds, labels)
+            prec.update(preds, labels)
+            recall.update(preds, labels)
+            acc.update(preds, labels)
+            all_preds.extend(preds.cpu().detach().numpy())
+            all_labels.extend(labels.cpu().numpy())
+    
+    avg_loss = loss / len(loader)
+    accu = float(num_corr) / float(num_samp)
+    pauc = pauc_above_tpr(all_labels, all_preds)
+    
+    print(f"Total loss: {loss}, Average loss: {avg_loss}")
+    print(f"Got {num_corr}/{num_samp} correct with accuracy {accu*100:.2f}")
+    print(f"pAUC above 80% TPR: {pauc:.3f}, Accuracy: {acc.compute().item():.3f}, precision: {prec.compute().item():.3f}, recall: {recall.compute().item():.3f}, F1Score: {metric.compute().item():.3f}")
+    model.train()
+
+    return avg_loss, acc.compute().item(), prec.compute().item(), recall.compute().item(), metric.compute().item(), pauc
+
+
+def save_checkpoint(state, filename="my_checkpoint.pth"):
+    '''
+    To save model while training 
+    Saves in working directory by default
+    '''
+    print("=> Saving checkpoint")
+    torch.save(state, filename)
+    print(f"Model Saved at {filename}")
+
+
+def load_checkpoint(checkpoint, model):
+    '''
+    
+    '''
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+
+
+def load_model(model_save_path):   # Use this for submission rather than load_checkpoint() defined above
+    '''
+    To load model during evaluation on test set
+    '''
+    model = torchvision.models.resnet34(weights=None)
+    num_ftrs = model.fc.in_features
+    model.fc = torch.nn.Linear(in_features=num_ftrs, out_features=1)
+    model.load_state_dict(torch.load(model_save_path)["state_dict"])
+    model.to(DEVICE)
+    model.eval()
+
+    return model
+
+
+def create_submission(model, test_loader, submission_file_path):
+    '''
+    To predict class probabilities on test data and generate submission.csv file
+    '''
+    predictions = []
+    image_ids = []
+
+    with torch.no_grad():
+        for inputs, image_names in tqdm(test_loader, desc="Evaluating"):
+            inputs = inputs.to(DEVICE)
+            outputs = model(inputs).squeeze(1)
+            probs = torch.sigmoid(outputs)
+            predictions.extend(probs.cpu().numpy())
+            image_ids.extend(image_names)  # Append all image names from the batch
+
+    # Check if the lengths match
+    if len(image_ids) != len(predictions):
+        print(f"Warning: Number of image IDs ({len(image_ids)}) does not match number of predictions ({len(predictions)})")
+
+    # Create DataFrame
+    submission_df = pd.DataFrame({
+        'isic_id': image_ids,
+        'target': predictions
+    })
+
+    # Save to CSV
+    submission_df.to_csv(submission_file_path, index=False)
+    print(f"Submission file saved to {submission_file_path}")
+
+
+def visualize_train_images(images, titles=None):
+    '''
+    
+    '''
+    plt.figure(figsize=(15, 5))
+    for i, image in enumerate(images):
+        plt.subplot(1, len(images), i + 1)
+        if isinstance(image, torch.Tensor):
+            image = image.permute(1, 2, 0).numpy()  # Change from CxHxW to HxWxC and convert to numpy
+        plt.imshow(image)
+        if titles:
+            plt.title(f"{titles[0][i]} (label: {titles[1][i]})")
+        plt.axis('off')
+    plt.show()
+    """ Usage
+    indices = np.random.choice(len(train_dataset_album), size=3, replace=False)
+    images, label, image_ids  = zip(*[train_dataset_album[i] for i in indices])
+    visualize_train_images(images, titles=[image_ids, label])
+    """
+
+
+def visualize_train_images(images, titles=None):
+    '''
+    
+    '''
+    plt.figure(figsize=(15, 5))
+    for i, image in enumerate(images):
+        plt.subplot(1, len(images), i + 1)
+        if isinstance(image, torch.Tensor):
+            image = image.permute(1, 2, 0).numpy()  # Change from CxHxW to HxWxC and convert to numpy
+        plt.imshow(image)
+        if titles:
+            plt.title(f"{titles[0][i]} (label: {titles[1][i]})")
+        plt.axis('off')
+    plt.show()
+
+    """ usage
+    indices = np.random.choice(len(train_dataset), size=3, replace=False)
+    images, label, image_ids  = zip(*[train_dataset[i] for i in indices])
+    visualize_train_images(images, titles=[image_ids, label])
+    """
+
+
+def visualize_test_images(images, titles=None):
+    '''
+    
+    '''
+    plt.figure(figsize=(15, 5))
+    for i, image in enumerate(images):
+        plt.subplot(1, len(images), i + 1)
+        if isinstance(image, torch.Tensor):
+            image = image.permute(1, 2, 0).numpy()  # Change from CxHxW to HxWxC and convert to numpy
+        plt.imshow(image)
+        if titles:
+            plt.title(titles[i])
+        plt.axis('off')
+    plt.show()
+    ''' Usage
+    indices = np.random.choice(len(test_dataset), size=3, replace=False)
+    images, image_ids  = zip(*[test_dataset[i] for i in indices])
+    visualize_test_images(images, titles=image_ids)    
+    '''
